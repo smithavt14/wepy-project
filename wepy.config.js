0e869e077362b1ef1187fe9751598fab64eb0784
Jasmine Tang <jastangting@gmail.com>
2018-09-04 18:49:31 +0800

    updated-venue-page

--- wepy.config.js
+++ wepy.config.js
@@ -1,4 +1,5 @@
 const path = require('path');
+const LessPluginAutoPrefix = require('less-plugin-autoprefix');
 var prod = process.env.NODE_ENV === 'production';
 
 module.exports = {
@@ -72,3 +73,9 @@ if (prod) {
     }
   }
 }
+
+compilers: {
+    less: {
+      compress: true,
+      plugins: [new LessPluginAutoPrefix({browsers: ['Android >= 2.3', 'Chrome > 20', 'iOS >= 6']})]
+    }
